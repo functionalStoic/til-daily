3a61f731358d2a50ef2bfb1509d5f5861b389312
Jason Sooter <jason.sooter@gmail.com>
2019-08-10 16:07:58 -0500

    feat(http-requests): switch from request -> axios - await Twilio command so that it runs

--- handler.js
+++ handler.js
@@ -1,4 +1,8 @@
 'use strict';
+const random = require('lodash.random');
+const axios = require('axios');
+const twilio = require('twilio');
+
 const {
   TWILIO_SID,
   TWILIO_TOKEN,
@@ -6,35 +10,29 @@ const {
   TRELLO_KEY,
   TRELLO_TOKEN,
 } = process.env;
-const request = require('request');
-const random = require('lodash.random');
-const client = new require('twilio')(TWILIO_SID, TWILIO_TOKEN);
 
-module.exports.tilDaily = async () =>
-  request(
-    {
-      method: 'GET',
-      url: `https://trello.com/1/boards/${TRELLO_BOARD_ID}/cards/visible`,
-      qs: { key: TRELLO_KEY, token: TRELLO_TOKEN },
-      headers: { 'cache-control': 'no-cache' },
-    },
-    (err, res, body) => {
-      if (err) throw new Error(err);
-      const parsed = JSON.parse(body);
-      const { name: title, shortUrl } = parsed[random(0, parsed.length)];
+const client = new twilio(TWILIO_SID, TWILIO_TOKEN);
 
-      // Send to Twilio
-      client.messages
-        .create({
-          body: `${title}} - ${shortUrl}`,
-          to: '+19187703878',
-          from: '+19184171839',
-        })
-        .then(({ sid }) => {
-          return {
-            statusCode: 200,
-            body: JSON.stringify({ sid }, null, 2),
-          };
-        });
-    },
-  );
+module.exports.tilDaily = async () => {
+  try {
+    // Get visible Trello cards
+    const { data } = await axios.get(
+      `https://trello.com/1/boards/${TRELLO_BOARD_ID}/cards/visible`,
+      {
+        params: { key: TRELLO_KEY, token: TRELLO_TOKEN },
+      },
+    );
+    const { name: title, shortUrl } = data[random(0, data.length)];
+    console.log({ title, shortUrl });
+
+    // Send to Twilio
+    const { sid, dateCreated, body } = await client.messages.create({
+      body: `${title}} - ${shortUrl}`,
+      to: '+19187703878',
+      from: '+19184171839',
+    });
+    console.log({ sid, dateCreated, body });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
